021057555f9d89ccbab3201d8712e957abff02c3
Sean Parker <seanrichardparker@gmail.com>
2019-05-22 16:11:05 -0400

    toggleDone setup

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = new Vue({
                   <div class="ui grid">
                     <div class="left floated twelve wide column">
                       <div class="ui checkbox">
-                        <input type="checkbox" name="task" :checked="task.completed">
+                        <input type="checkbox" name="task" v-on:click="$parent.toggleDone($event, task.id)" :checked="task.completed" >
                         <label>{{task.name}} - <span>{{ task.description }}</span></label>
                       </div>
                     </div>  
@@ -58,6 +58,14 @@ var app = new Vue({
 
     todoTasks: function() {
       return this.tasks.filter(item => item.completed == false);
+    }
+  },
+  methods: {
+    toggleDone: function(event, id) {
+      // let task = this.tasks.find(item => item.id == id);
+      // console.log("task toggled");
+      console.log(event);
+      console.log(id);
     }
   }
 });
