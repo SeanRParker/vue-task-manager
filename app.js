1054651806876202640e9dd500eb2a10d7ffa8cc
Sean Parker <seanrichardparker@gmail.com>
2019-05-22 14:08:07 -0400

    looping tasks (why v-fors are awesome)

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 var app = new Vue({
   el: "#app",
   data: {
+    tasks: [
+      {
+        id: 1,
+        name: "Todo 1",
+        description: "This is a todo",
+        completed: false
+      },
+      {
+        id: 2,
+        name: "Todo 2",
+        description: "This is a another todo",
+        completed: false
+      },
+      {
+        id: 3,
+        name: "Todo 3",
+        description: "This is a complete todo",
+        completed: true
+      },
+      {
+        id: 4,
+        name: "Todo 4",
+        description: "This is a another complete todo",
+        completed: true
+      }
+    ],
     message: "Hello World!"
   }
 });
